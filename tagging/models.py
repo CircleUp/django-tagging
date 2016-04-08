9132791d59a925bb6c6fc84a542d8326a53fd058
Jeremy Greenfield <jgreenfield@circleup.com>
2016-04-07 17:32:26 -0700

    Fix another 1.9 issue

--- tagging/models.py
+++ tagging/models.py
@@ -7,7 +7,7 @@ try:
 except NameError:
     from sets import Set as set
 
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes import fields
 from django.contrib.contenttypes.models import ContentType
 from django.db import connection, models
 from django.db.utils import IntegrityError
@@ -486,7 +486,7 @@ class TaggedItem(models.Model):
     tag          = models.ForeignKey(Tag, verbose_name=_('tag'), related_name='items')
     content_type = models.ForeignKey(ContentType, verbose_name=_('content type'))
     object_id    = models.PositiveIntegerField(_('object id'), db_index=True)
-    object       = generic.GenericForeignKey('content_type', 'object_id')
+    object       = fields.GenericForeignKey('content_type', 'object_id')
 
     objects = TaggedItemManager()
 
