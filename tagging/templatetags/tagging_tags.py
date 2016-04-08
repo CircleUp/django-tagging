cd08353b8829a8d7cc0a682eb94d094b593c638c
Jeremy Greenfield <jgreenfield@circleup.com>
2016-04-07 17:26:52 -0700

    Update for django 1.9 and increment version

--- tagging/templatetags/tagging_tags.py
+++ tagging/templatetags/tagging_tags.py
@@ -1,4 +1,4 @@
-from django.db.models import get_model
+from django.apps import apps
 from django.template import Library, Node, TemplateSyntaxError, Variable, resolve_variable
 from django.utils.translation import ugettext as _
 
@@ -14,7 +14,7 @@ class TagsForModelNode(Node):
         self.counts = counts
 
     def render(self, context):
-        model = get_model(*self.model.split('.'))
+        model = apps.get_model(*self.model.split('.'))
         if model is None:
             raise TemplateSyntaxError(_('tags_for_model tag was given an invalid model: %s') % self.model)
         context[self.context_var] = Tag.objects.usage_for_model(model, counts=self.counts)
@@ -27,7 +27,7 @@ class TagCloudForModelNode(Node):
         self.kwargs = kwargs
 
     def render(self, context):
-        model = get_model(*self.model.split('.'))
+        model = apps.get_model(*self.model.split('.'))
         if model is None:
             raise TemplateSyntaxError(_('tag_cloud_for_model tag was given an invalid model: %s') % self.model)
         context[self.context_var] = \
@@ -51,7 +51,7 @@ class TaggedObjectsNode(Node):
         self.model = model
 
     def render(self, context):
-        model = get_model(*self.model.split('.'))
+        model = apps.get_model(*self.model.split('.'))
         if model is None:
             raise TemplateSyntaxError(_('tagged_objects tag was given an invalid model: %s') % self.model)
         context[self.context_var] = \
